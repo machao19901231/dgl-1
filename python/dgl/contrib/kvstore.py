daa65040116e9095f85e9b964ad7db5533e42c75
Da Zheng <zhengda1936@gmail.com>
2019-02-20 13:04:30 -0800

    add update_all.

--- python/dgl/contrib/kvstore.py
+++ python/dgl/contrib/kvstore.py
@@ -1,6 +1,46 @@
 import os
+import sys
 import dgl
 import mxnet as mx
+import pickle
+import numpy as np
+from dgl.data import load_data
+from mxnet import gluon
+
+if sys.version_info[0] > 2:
+    # this function is needed for python3
+    # to convert ctypes.char_p .value back to python str
+    py_str = lambda x: x.decode('utf-8')
+else:
+    py_str = lambda x: x
+
+class Arguments(object):
+    def __init__(self, dataset):
+        self.dataset = dataset
+
+def load_graph(graph_name):
+    args = Arguments(graph_name)
+    data = load_data(args)
+    return data.graph
+
+
+def _remove_params(block):
+    ret = {}
+    params = block.collect_params()
+    for name in params:
+        param = params[name]
+        ret.update({name : param._reduce()})
+        param._data = None
+        param._grad = None
+    return ret
+
+
+def _add_params(block, param_arrays):
+    params = block.collect_params()
+    for name in params:
+        param = params[name]
+        param._init_impl(param_arrays[name], param._ctx_list)
+
 
 class Optimizer(object):
     def __init__(self, graph_name):
@@ -15,16 +55,23 @@ class Optimizer(object):
         if isinstance(weight, mx.nd.sparse.RowSparseNDArray):
             data = mx.nd.ones(shape=weight.shape, dtype=weight.dtype)
             data.copyto(weight)
+            self.graph.ndata[index] = weight
         self.weights.update({index : weight})
         return self
 
     def update_multi_precision(self, index, weight, grad, state):
         print("update multi-precision: " + index)
         # Update all node embeddings
-        if index.startswith("dgl:update_all:layer-"):
-            layer_id = get_layer_id(index)
-            self.graph.update_all(self.msg_funcs[layer_id], self.reduce_funcs[layer_id],
-                                  self.update_funcs[layer_id])
+        if index.startswith("dgl:update_all"):
+            mfunc, rfunc, ufunc = pickle.loads(grad.asnumpy().tostring())
+            if isinstance(mfunc, gluon.Block):
+                _add_params(mfunc, self.weights)
+            if isinstance(rfunc, gluon.Block):
+                _add_params(rfunc, self.weights)
+            if isinstance(ufunc, gluon.Block):
+                _add_params(ufunc, self.weights)
+
+            self.graph.update_all(mfunc, rfunc, ufunc)
         # Update node embeddings
         elif isinstance(weight, mx.nd.sparse.RowSparseNDArray):
             #TODO this isn't a right way to copy data.
@@ -35,10 +82,10 @@ class Optimizer(object):
         
 
 class DGLKVstore(object):
-    def __init__(self, embed_dict):
+    def __init__(self, graph_name, embed_dict):
         self._kv = mx.kv.create('local')
         #self._kv = mx.kv.create('dist_sync')
-        self._kv.set_optimizer(Optimizer())
+        self._kv.set_optimizer(Optimizer(graph_name))
         self._embed_dict = embed_dict
         for name in embed_dict:
             shape = embed_dict[name]['shape']
@@ -48,6 +95,9 @@ class DGLKVstore(object):
                                                       shape=shape, dtype=dtype)
             self._kv.init(name, init_data)
             self._kv.push(name, init_data)
+
+        self._kv.init("dgl:update_all", mx.nd.zeros(shape=(1000), dtype=np.uint8))
+        self._params = {}
 
     def pull(self, name, row_ids):
         row_ids = mx.nd.array(row_ids, dtype='int64')
@@ -61,3 +111,30 @@ class DGLKVstore(object):
                                                           shape=self._embed_dict[name]['shape'],
                                                           dtype=self._embed_dict[name]['dtype']))
 
+    def _push_params(self, params):
+        for name in params:
+            if name not in self._params:
+                self._kv.init(name, params[name])
+            self._kv.push(name, params[name])
+
+    def update_all(self, msg_func, reduce_func, update_func):
+        if isinstance(msg_func, gluon.Block):
+            msg_params = _remove_params(msg_func)
+            self._push_params(msg_params)
+        if isinstance(reduce_func, gluon.Block):
+            reduce_params = _remove_params(reduce_func)
+            self._push_params(reduce_params)
+        if isinstance(update_func, gluon.Block):
+            update_params = _remove_params(update_func)
+            self._push_params(update_params)
+
+        func_str = py_str(pickle.dumps((msg_func, reduce_func, update_func), 0))
+        arr = np.fromstring(func_str, dtype=np.uint8)
+        self._kv.push("dgl:update_all", mx.nd.array(arr, dtype=np.uint8))
+
+        if isinstance(msg_func, gluon.Block):
+            _add_params(msg_func, msg_params)
+        if isinstance(reduce_func, gluon.Block):
+            _add_params(reduce_func, reduce_params)
+        if isinstance(update_func, gluon.Block):
+            _add_params(update_func, update_params)
--- tests/mxnet/test_kvstore.py
+++ tests/mxnet/test_kvstore.py
@@ -1,11 +1,40 @@
 import dgl
 import mxnet as mx
+from mxnet import gluon
+from dgl.data import load_data
 
-embeddings = {'a' : {'shape' : (10, 10), 'dtype' : 'float32'}}
-kv = dgl.contrib.DGLKVstore(embeddings)
+class Arguments(object):
+    def __init__(self, dataset):
+        self.dataset = dataset
+
+data = load_data(Arguments('citeseer'))
+g = dgl.DGLGraph(data.graph)
+
+embeddings = {'h' : {'shape' : (3327, 10), 'dtype' : 'float32'}}
+kv = dgl.contrib.DGLKVstore('citeseer', embeddings)
 data = mx.nd.random.normal(shape=(5, 10))
-print(data)
-kv.push('a', [0, 1, 2, 3, 4], data)
-kv.push('a', [1, 2, 3, 4, 5], data)
-print(kv.pull('a', [0, 2, 5]))
-print(kv.pull('a', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
+kv.push('h', [0, 1, 2, 3, 4], data)
+kv.push('h', [1, 2, 3, 4, 5], data)
+print(kv.pull('h', [0, 2, 5]))
+print(kv.pull('h', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
+
+def msg_func(edges):
+    return {'m': edges.src['h']}
+
+def reduce_func(nodes):
+    return {'h' : mx.nd.sum(nodes.mailbox['m'])}
+
+class NodeUpdate(gluon.Block):
+    def __init__(self, out_feats, activation=None, bias=True):
+        super(NodeUpdate, self).__init__()
+        with self.name_scope():
+            self.linear = gluon.nn.Dense(out_feats, use_bias=bias, activation=activation)
+
+    def forward(self, node):
+        return {'h': self.linear(node.data['h'])}
+
+update = NodeUpdate(10)
+update.initialize(ctx=mx.cpu())
+g.ndata['h'] = mx.nd.ones((g.number_of_nodes(), 10))
+g.update_all(msg_func, reduce_func, update)
+kv.update_all(msg_func, reduce_func, update)
