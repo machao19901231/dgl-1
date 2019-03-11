353bb30aedebc374a8cae9757f700f78c8d7ec4b
aksnzhy <mctt90@gmail.com>
2019-03-11 16:56:03 +0800

    update

--- examples/mxnet/sampling/distributed_sampling/NON_NUMA/run_sampler.sh
+++ examples/mxnet/sampling/distributed_sampling/NON_NUMA/run_sampler.sh
@@ -1,11 +1,1 @@
-# Start 10 processes
-python3 sampler.py &
-python3 sampler.py &
-python3 sampler.py &
-python3 sampler.py &
-python3 sampler.py &
-python3 sampler.py &
-python3 sampler.py &
-python3 sampler.py &
-python3 sampler.py &
-python3 sampler.py
+DGLBACKEND=mxnet python3 sampler.py --dataset reddit-self-loop --num-neighbors 2 --batch-size 1000 --test-batch-size 500 --n-hidden 64
