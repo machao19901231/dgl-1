fa4bd21e1b28455cf4501212c50dd4fc6a83adb2
aksnzhy <mctt90@gmail.com>
2019-03-14 11:56:51 +0800

    update

--- examples/mxnet/sampling/distributed_sampling/NON_NUMA/run_sampler.sh
+++ examples/mxnet/sampling/distributed_sampling/NON_NUMA/run_sampler.sh
@@ -1,2 +1,4 @@
 # Start 3 sampler process
+DGLBACKEND=mxnet python3 sampler.py --dataset reddit-self-loop --num-neighbors 2 --batch-size 1000 --test-batch-size 500 &
+DGLBACKEND=mxnet python3 sampler.py --dataset reddit-self-loop --num-neighbors 2 --batch-size 1000 --test-batch-size 500 &
 DGLBACKEND=mxnet python3 sampler.py --dataset reddit-self-loop --num-neighbors 2 --batch-size 1000 --test-batch-size 500
